b754deae36545a23833077471482ab1296016944
pleasewaitnow <amirreza.javaherdashti@proton.me>
2023-07-23 16:08:17 +0200

    t1 :: write the base functions

--- app/Http/Controllers/ImageModificationService.php
+++ app/Http/Controllers/ImageModificationService.php
@@ -2,20 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use Imagick;
+
 class ImageModificationService
 {
-    public function modify(string $file, array $modifiers): string
+    public function modify(string $source, array $modifiers): string
     {
         if (empty($modifiers)) {
-            return $file;
+            return $source;
         }
 
-        // generate a new file
+        $destination = public_path() . $this->generateFileName();
 
         return $this->generateFileName();
     }
 
-    function generateFileName(string $prefix = 'generated-image'): string
+    private function resize(string $source, string $destination, string $height, string $width): void
+    {
+        $imagick = new Imagick($source);
+        $imagick->cropImage($width, $height, 0, 0);
+        $imagick->writeImage($destination);
+
+        $imagick->clear();
+        $imagick->destroy();
+    }
+
+    private function crop(string $source, string $destination, string $height, string $width): void
+    {
+
+    }
+
+    private function generateFileName(string $prefix = 'generated-image'): string
     {
         return sprintf('%s-%s', $prefix, time());
     }
