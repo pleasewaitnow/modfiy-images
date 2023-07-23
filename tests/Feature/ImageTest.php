9c95d55f2d44193b5424d130a91e84ae22417ce3
pleasewaitnow <amirreza.javaherdashti@proton.me>
2023-07-23 17:01:44 +0200

    t1 :: test the redirects

--- tests/Feature/ImageTest.php
+++ tests/Feature/ImageTest.php
@@ -7,10 +7,32 @@ use Tests\TestCase;
 
 class ImageTest extends TestCase
 {
-    public function test_example()
+    public function testSuccessful()
     {
-        $endpoint = route('get.images.modify', ['file' => 'name.png']);
+        $endpoint = route('get.images.modify', ['file' => 'xbox.png']);
 
         $this->get($endpoint)->assertStatus(200);
+    }
+
+    public function testInvalidQueryParameters()
+    {
+        $endpoint = route('get.images.modify', [
+            'file' => 'xbox.png',
+            'unknown_height' => 500,
+            'unknown_width' => 200
+        ]);
+
+        $this->get($endpoint)->assertStatus(500);
+    }
+
+    public function testRedirectWithValidParameters()
+    {
+        $endpoint = route('get.images.modify', [
+            'file' => 'xbox.png',
+            'crop_height' => 500,
+            'crop_width' => 200
+        ]);
+
+        $this->get($endpoint)->assertStatus(302);
     }
 }
